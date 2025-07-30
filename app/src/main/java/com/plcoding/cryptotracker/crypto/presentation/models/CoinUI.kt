e594bc987b9829f53c6f4d6aeb1e03b77ce4c6ee
Lakshaydang47 <lakshaydang764@gmail.com>
2025-07-30 21:45:43 +0530

    feat: Add Coin data class and IntelliJ project files
    This commit introduces the `Coin` data class, which represents a cryptocurrency with properties like ID, rank, name, symbol, market capitalization, price, and 24-hour change percentage.
    
    Additionally, various IntelliJ IDEA project configuration files (`.idea/`) have been added. These files manage project settings related to:
    
    - Version control (Git)
    - Android project system
    - Project name
    - Compiler settings (bytecode target level 21)
    - App Insights (Firebase Crashlytics placeholder)
    - Ignored files for IDEA
    - Project migrations
    - Run configurations
    - Miscellaneous project settings (JDK, output path)
    - Gradle settings (test runner, external project path, Gradle JVM)

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUI.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUI.kt
@@ -1,6 +1,10 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
 import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
 
 data class CoinUI(
     val id: String,
@@ -17,3 +21,27 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinUI(): CoinUI {
+    return CoinUI(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply{
+        maximumFractionDigits = 2
+        minimumFractionDigits = 2
+    }
+    return  DisplayableNumber(
+        value = this,
+        formatted =  formatter.format(this)
+    )
+}
